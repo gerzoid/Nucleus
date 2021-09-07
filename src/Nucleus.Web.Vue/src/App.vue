f9593f187de96e5e525c709db99bbbf394d2e73f
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2021-09-07 19:05:47 +0300

    Re-created vue project

--- src/Nucleus.Web.Vue/src/App.vue
+++ src/Nucleus.Web.Vue/src/App.vue
@@ -1,17 +1,30 @@
 <template>
-    <v-app>
-        <router-view></router-view>
-        <v-layout row justify-center>
-            <v-dialog v-model="nucleus.isLoading" persistent content content-class="centered-dialog">
-                <v-container fill-height>
-                    <v-layout column justify-center align-center>
-                        <v-progress-circular indeterminate :size="70" :width="7" :color="'primary'"></v-progress-circular>
-                        <h1>Loading...</h1>
-                    </v-layout>
-                </v-container>
-            </v-dialog>
-        </v-layout>
-    </v-app>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
 
-<script src="./App.ts"></script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
--- src/Nucleus.Web.Vue/src/main.ts
+++ src/Nucleus.Web.Vue/src/main.ts
@@ -1,30 +1,6 @@
-import './assets/sass/site.css';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import vuetify from './plugins/vuetify';
-import VueI18n from 'vue-i18n';
-import LanguageStore from '@/stores/language-store';
-
-Vue.use(VueI18n);
-
-const locales = {
-    en: require('@/assets/localizations/en.json'),
-    tr: require('@/assets/localizations/tr.json')
-};
-
-const i18n = new VueI18n({
-    locale: LanguageStore.getLanguage().languageCode,
-    fallbackLocale: 'en',
-    messages: locales
-});
-
-Vue.config.productionTip = false;
-
-new Vue({
-    i18n,
-    router,
-    vuetify,
-    render: h => h(App)
-}).$mount('#app');
+createApp(App).use(store).use(router).mount('#app')
